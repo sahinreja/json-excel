d0a74527af705d1a8683b48f9f5817290920dbc5
Sahin Reja <sahinreja512@gmail.com>
2021-07-22 12:14:12 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as XLSX from 'xlsx';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exit';
+  student:any;
+  fileName = 'StudentExcel.xlsx'
+  constructor(private http:HttpClient){
+    this.getStudentData();
+  }
+
+  getStudentData(){
+    this.http.get<any>('../assets/student.json').subscribe((res)=>{
+      console.log(res);
+      this.student = res;
+    },
+    (err)=>{
+      console.log(err);
+    })
+  }
+
+  export(){
+    let el = document.getElementById('table')
+    const worksheet: XLSX.WorkSheet =XLSX.utils.table_to_sheet(el);
+
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    XLSX.writeFile(workbook, this.fileName);
+  }
+
 }
